51800abdb7c5d602e4940ce4400e527f832428e1
Jolomi Taiwo <taiwojolomi15@gmail.com>
2025-02-07 11:41:48 +0100

    fix: correct className formatting in Messages component

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -7,7 +7,7 @@ interface MessagesProps {
 }
 const Messages = ({ messages }: MessagesProps) => {
 	return (
-		<div className="flexmax-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
+		<div className="flex max-h-[calc(100vh-3.5rem-7rem)] flex-1 flex-col overflow-y-auto">
 			{messages.length ? (
 				messages.map((message, i) => (
 					<Message
